97fb6505c796262159e098bb5a30ca7bdd2c9e69
Luboš Luňák <l.lunak@centrum.cz>
2023-04-07 20:33:08 +0200

    implement thresholds for storage bin and refrigerator

---
+++ Source/Storage.cs
@@ -0,0 +1,220 @@
+using HarmonyLib;
+using UnityEngine;
+using KSerialization;
+using STRINGS;
+using System.Reflection;
+
+namespace StorageRefrigeratorThresholds
+{
+    // Implementing thresholds requires using IActivateRangeTarget for a class.
+    // Since StorageLockerSmart and Refrigerator cannot be changed, create
+    // a new class to handle this, patch the original classes to communicate
+    // with this one.
+    // The rest is using the same logic as BatterySmart or SmartReservoir,
+    // only activate and deactivate must be swapped (since here green means full).
+    public abstract class ThresholdsBase : KMonoBehaviour, IActivationRangeTarget
+    {
+        [Serialize]
+        private int activateValue = 100;
+
+        [Serialize]
+        private int deactivateValue = 0;
+
+        [Serialize]
+        private bool activated;
+
+        public float ActivateValue
+        {
+            get
+            {
+                return activateValue;
+            }
+            set
+            {
+                activateValue = (int)value;
+                UpdateLogicCircuit();
+            }
+        }
+
+        public float DeactivateValue
+        {
+            get
+            {
+                return deactivateValue;
+            }
+            set
+            {
+                deactivateValue = (int)value;
+                UpdateLogicCircuit();
+            }
+        }
+
+        public float MinValue => 0f;
+
+        public float MaxValue => 100f;
+
+        public bool UseWholeNumbers => true;
+
+        public string ActivateTooltip => STRINGS.STORAGEREFRIGERATORTHRESHOLDS.ACTIVATE_TOOLTIP;
+
+        public string DeactivateTooltip => STRINGS.STORAGEREFRIGERATORTHRESHOLDS.DEACTIVATE_TOOLTIP;
+
+        // These strings are reusable, except that activate and deactivate are inverted.
+        public string ActivationRangeTitleText => BUILDINGS.PREFABS.SMARTRESERVOIR.SIDESCREEN_TITLE;
+
+        public string ActivateSliderLabelText => BUILDINGS.PREFABS.SMARTRESERVOIR.SIDESCREEN_DEACTIVATE;
+
+        public string DeactivateSliderLabelText => BUILDINGS.PREFABS.SMARTRESERVOIR.SIDESCREEN_ACTIVATE;
+
+        public bool UpdateLogicState(float percentFull)
+        {
+            float num = Mathf.RoundToInt(percentFull * 100f);
+            if (activated)
+            {
+                if (num <= (float)deactivateValue)
+                    activated = false;
+            }
+            else if (num >= (float)activateValue)
+                activated = true;
+            return activated;
+        }
+
+        protected abstract void UpdateLogicCircuit();
+    }
+
+    public class StorageThresholds : ThresholdsBase
+    {
+        private static readonly MethodInfo updateLogicAndActiveStateMethod
+            = AccessTools.Method(typeof(StorageLockerSmart),"UpdateLogicAndActiveState");
+
+        protected override void UpdateLogicCircuit()
+        {
+            updateLogicAndActiveStateMethod.Invoke( GetComponent<StorageLockerSmart>(), null );
+        }
+    }
+
+    // OnCopySettings() is inherited from StorageLocker in StorageLockerSmart,
+    // so it needs to be patched there. For normal StorageLocker objects it will not change
+    // anything, because they do not include StorageThresholds.
+    [HarmonyPatch(typeof(StorageLocker))]
+    public class StorageLocker_Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(OnCopySettings))]
+        public static void OnCopySettings(StorageLockerSmart __instance, object data)
+        {
+            GameObject otherGameObject = (GameObject)data;
+            if (otherGameObject != null)
+            {
+                StorageThresholds component = __instance.gameObject.GetComponent<StorageThresholds>();
+                StorageThresholds otherComponent = otherGameObject.GetComponent<StorageThresholds>();
+                if (component != null && otherComponent != null)
+                {
+                    component.ActivateValue = otherComponent.ActivateValue;
+                    component.DeactivateValue = otherComponent.DeactivateValue;
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(StorageLockerSmart))]
+    public class StorageLockerSmart_Patch
+    {
+        private static readonly MethodInfo getAmountStoredMethod
+            = AccessTools.Method(typeof(FilteredStorage),"GetAmountStored");
+        private static readonly MethodInfo getMaxCapacityMethod
+         = AccessTools.Method(typeof(FilteredStorage),"GetMaxCapacityMinusStorageMargin");
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(UpdateLogicAndActiveState))]
+        public static bool UpdateLogicAndActiveState(StorageLockerSmart __instance, FilteredStorage ___filteredStorage,
+            Operational ___operational, LogicPorts ___ports)
+        {
+            StorageThresholds component = __instance.gameObject.GetComponent<StorageThresholds>();
+            float stored = (float) getAmountStoredMethod.Invoke(___filteredStorage, null );
+            float capacity = (float) getMaxCapacityMethod.Invoke(___filteredStorage, null );
+            bool num = component.UpdateLogicState( stored / capacity );
+            bool isOperational = ___operational.IsOperational;
+            bool flag = num && isOperational;
+            ___ports.SendSignal(FilteredStorage.FULL_PORT_ID, flag ? 1 : 0);
+            ___filteredStorage.SetLogicMeter(flag);
+            ___operational.SetActive(isOperational);
+            return false; // skip the original
+        }
+    }
+
+    [HarmonyPatch(typeof(StorageLockerSmartConfig))]
+    public class StorageLockerSmartConfig_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(DoPostConfigureComplete))]
+        public static void DoPostConfigureComplete(GameObject go)
+        {
+            go.AddOrGet<StorageThresholds>();
+        }
+    }
+
+    public class RefrigeratorThresholds : ThresholdsBase
+    {
+        private static readonly MethodInfo updateLogicCircuitMethod
+            = AccessTools.Method(typeof(Refrigerator),"UpdateLogicCircuit");
+
+        protected override void UpdateLogicCircuit()
+        {
+            updateLogicCircuitMethod.Invoke( GetComponent<Refrigerator>(), null );
+        }
+    }
+
+    [HarmonyPatch(typeof(Refrigerator))]
+    public class Refrigerator_Patch
+    {
+        private static readonly MethodInfo getAmountStoredMethod
+            = AccessTools.Method(typeof(FilteredStorage),"GetAmountStored");
+        private static readonly MethodInfo getMaxCapacityMethod
+         = AccessTools.Method(typeof(FilteredStorage),"GetMaxCapacityMinusStorageMargin");
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(OnCopySettings))]
+        public static void OnCopySettings(Refrigerator __instance, object data)
+        {
+            GameObject otherGameObject = (GameObject)data;
+            if (otherGameObject != null)
+            {
+                RefrigeratorThresholds component = __instance.gameObject.GetComponent<RefrigeratorThresholds>();
+                RefrigeratorThresholds otherComponent = otherGameObject.GetComponent<RefrigeratorThresholds>();
+                if (component != null && otherComponent != null)
+                {
+                    component.ActivateValue = otherComponent.ActivateValue;
+                    component.DeactivateValue = otherComponent.DeactivateValue;
+                }
+            }
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(UpdateLogicCircuit))]
+        public static bool UpdateLogicCircuit(Refrigerator __instance, FilteredStorage ___filteredStorage,
+            Operational ___operational, LogicPorts ___ports)
+        {
+            RefrigeratorThresholds component = __instance.gameObject.GetComponent<RefrigeratorThresholds>();
+            float stored = (float) getAmountStoredMethod.Invoke(___filteredStorage, null );
+            float capacity = (float) getMaxCapacityMethod.Invoke(___filteredStorage, null );
+            bool num = component.UpdateLogicState( stored / capacity );
+            bool isOperational = ___operational.IsOperational;
+            bool flag = num && isOperational;
+            ___ports.SendSignal(FilteredStorage.FULL_PORT_ID, flag ? 1 : 0);
+            ___filteredStorage.SetLogicMeter(flag);
+            return false; // skip the original
+        }
+    }
+
+    [HarmonyPatch(typeof(RefrigeratorConfig))]
+    public class RefrigeratorConfig_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(DoPostConfigureComplete))]
+        public static void DoPostConfigureComplete(GameObject go)
+        {
+            go.AddOrGet<RefrigeratorThresholds>();
+        }
+    }
+}
