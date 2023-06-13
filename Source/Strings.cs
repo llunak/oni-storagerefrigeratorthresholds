ccacb09e133b069da90f1c1d54cf0fc14cbb7648
Luboš Luňák <l.lunak@centrum.cz>
2023-06-13 21:30:47 +0200

    fix inverted threshold names in tooltips

--- Source/Strings.cs
+++ Source/Strings.cs
@@ -4,8 +4,8 @@ namespace STRINGS
 {
     public class STORAGEREFRIGERATORTHRESHOLDS
     {
-        public static LocString ACTIVATE_TOOLTIP = "Sends a " + UI.FormatAsAutomationState("Green Signal", UI.AutomationState.Active) + " when storage is <b>{0}%</b> full, until it is less than <b>{1}% (High Threshold)</b> full";
+        public static LocString ACTIVATE_TOOLTIP = "Sends a " + UI.FormatAsAutomationState("Green Signal", UI.AutomationState.Active) + " when storage is <b>{0}%</b> full, until it is less than <b>{1}% (Low Threshold)</b> full";
 
-        public static LocString DEACTIVATE_TOOLTIP = "Sends a " + UI.FormatAsAutomationState("Red Signal", UI.AutomationState.Standby) + " when storage is less than <b>{0}%</b> full, until it is <b>{1}% (Low Threshold)</b> full";
+        public static LocString DEACTIVATE_TOOLTIP = "Sends a " + UI.FormatAsAutomationState("Red Signal", UI.AutomationState.Standby) + " when storage is less than <b>{0}%</b> full, until it is <b>{1}% (High Threshold)</b> full";
     }
 }
